7c245fba6159bf53bb7bc8d376ea670d6ba21815
Veronica Miller <vmcmiller@gmail.com>
2019-03-06 16:12:10 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -93,3 +93,70 @@ let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
 
 billy.matchInterests(freeMusic);
 // Uncaught TypeError: Cannot read property 'interests' of undefined
+
+//------------//
+
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        console.log("'this' is defined: ", this);
+        return event.keywords.some(function(word) {
+            console.log("'this' is now undefined: ", this);
+            return this.interests.includes(word);
+        });
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
+// 'this' is defined:  User {name: "billy", interests: Array(3)}
+// 'this' is now undefined:  undefined
+// Uncaught TypeError: Cannot read property 'interests' of undefined
+
+//------------//
+
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        return event.keywords.some(
+            function(word) {
+                return this.interests.includes(word);
+            }.bind(this) // added to the and of the callback function
+        );
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
+
+//------------//
+
+matchInterests(event) {
+  return event.keywords.some(word => this.interests.includes(word));
+}
